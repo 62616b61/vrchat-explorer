850be3b115c2f6ef9d6c5f799be41c7b1d7d38db
Alex Katsero <alexanderkatsero@gmail.com>
2022-01-17 13:41:39 +0200

    debug: auth via username+password and rewrite jar (test)

--- src/lib/vrchat-session-helper.js
+++ src/lib/vrchat-session-helper.js
@@ -11,48 +11,21 @@ export async function initializeVRChatSession () {
     throw new Error('You forgot to provide VRCHAT_AUTH_API_URL!');
   }
 
-  const cookieString = await globalAxios.defaults.jar.getCookieString(VRCHAT_API_URL);
+  //const cookieString = await globalAxios.defaults.jar.getCookieString(VRCHAT_API_URL);
 
   // Return if session cookies are already set.
-  if (cookieString.length > 0) {
-    return;
-  }
+  //if (cookieString.length > 0) {
+    //return;
+  //}
 
   try { 
     console.log("RETRIEVING SESSION FROM VRCHAT-AUTH-SERVICE")
     //const { data: session } = await axios.get(`${VRCHAT_AUTH_API_URL}/session`);
 
-    const jar_data = {
-      version: 'tough-cookie@4.0.0',
-      storeType: 'MemoryCookieStore',
-      rejectPublicSuffixes: true,
-      cookies: [
-        {
-          key: 'auth',
-          value: 'authcookie_f9d64bb6-c223-4b67-b9c2-f1d145a16408',
-          expires: '2022-01-24T10:18:48.000Z',
-          maxAge: 604800,
-          domain: 'api.vrchat.cloud',
-          path: '/',
-          httpOnly: true,
-          hostOnly: true,
-          creation: '2022-01-17T10:18:49.217Z',
-          lastAccessed: '2022-01-17T10:18:49.217Z'
-        },
-        {
-          key: 'apiKey',
-          value: 'JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26',
-          domain: 'api.vrchat.cloud',
-          path: '/',
-          hostOnly: true,
-          creation: '2022-01-17T10:18:49.217Z',
-          lastAccessed: '2022-01-17T10:18:49.217Z'
-        }
-      ]
-    }
-    const jar = CookieJar.deserializeSync(jar_data)
+    const oldJar = globalAxios.defaults.jar.serializeSync();
+    const newJar = CookieJar.deserializeSync(oldJar);
 
-    globalAxios.defaults.jar = jar
+    globalAxios.defaults.jar = newJar;
 
     const AuthenticationApi = new vrchat.AuthenticationApi();
     const user = await AuthenticationApi.getCurrentUser();
