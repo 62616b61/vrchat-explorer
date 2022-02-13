59e75adc47351ef8a80e98e546f07fa7a0b39c5a
Alex Katsero <alexanderkatsero@gmail.com>
2022-02-13 20:38:29 +0200

    infra: rename metrics timestream results topic

--- stacks/Metrics/sns.js
+++ stacks/Metrics/sns.js
@@ -1,7 +1,11 @@
 import { Topic } from "@serverless-stack/resources";
 
 export default function sns(scope) {
-  const timestreamScheduledQueryResultsTopic = new Topic(scope, "metrics-service-timestream-scheduled-query-results-topic");
+  const timestreamScheduledQueryResultsTopic = new Topic(scope, "metrics-service-timestream-scheduled-query-results-topic", {
+    snsTopic: {
+      topicName: scope.node.root.logicalPrefixedName("metrics-service-timestream-scheduled-query-results"),
+    },
+  });
 
   return { timestreamScheduledQueryResultsTopic };
 }
